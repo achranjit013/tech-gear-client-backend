d2319d98de16aad913b87d76d7ae54fa7c9dd45c
Ranjit Acharya <achranjit013@gmail.com>
2024-02-12 15:32:27 +1100

    fetch fresh products from database for cart and provide to frondend for display.

--- src/model/ProductModel.js
+++ src/model/ProductModel.js
@@ -17,6 +17,32 @@ export const getAProductBySlug = (slug) => {
   return collection.findOne({ slug });
 };
 
+export const getProductBySlugAndSize = (slug, size) => {
+  const query = {
+    slug,
+    "variants.size": size,
+  };
+
+  const options = {
+    projection: {
+      _id: 1,
+      status: 1,
+      name: 1,
+      parentCatId: 1,
+      slug: 1,
+      basePrice: 1,
+      sku: 1,
+      description: 1,
+      thumbnail: 1,
+      variants: {
+        $elemMatch: { size },
+      },
+    },
+  };
+
+  return collection.findOne(query, options);
+};
+
 export const getLatestArrivalProducts = () => {
   // optional params, only gets latest 12
   const options = {
--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -2,11 +2,30 @@ import express from "express";
 import {
   getAProductBySlug,
   getLatestArrivalProducts,
+  getProductBySlugAndSize,
 } from "../model/ProductModel.js";
 import { responder } from "../middlewares/response.js";
 
 const router = express.Router();
 
+// get products for cart by their id / slug and size
+router.get("/cart-item/:slug&:size", async (req, res, next) => {
+  try {
+    const { slug, size } = req.params;
+
+    const findResult = await getProductBySlugAndSize(slug, size);
+
+    responder.SUCESS({
+      res,
+      message: "successfully retrieved products",
+      findResult,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// get all products or one selected product
 router.get("/:slug?", async (req, res, next) => {
   try {
     // slug
@@ -15,7 +34,7 @@ router.get("/:slug?", async (req, res, next) => {
     const findResult = slug
       ? await getAProductBySlug(slug)
       : await getLatestArrivalProducts();
-
+    console.log(findResult);
     responder.SUCESS({
       res,
       message: "successfully retrieved products",
